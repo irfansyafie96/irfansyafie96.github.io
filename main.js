b1029db3c9062d504c5e0c87c9609565727efeff
irfansyafie96 <irfansyafie96@gmail.com>
2025-10-25 01:12:36 +0800

    Add dynamic blog post listing and improve styles
    Implemented dynamic loading of blog posts on the blog page using data from blog-posts.json. Added the first blog post image, updated blog post metadata, and improved blog and card styling for better UI consistency. Changed blog page title and ensured images display correctly in blog posts.

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
+// Load navigation bar on every page
 fetch("/components/navbar.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navBarContainer").innerHTML = data;
   });
+
+// Load blog posts on blog page
+document.addEventListener("DOMContentLoaded", function () {
+  // Check if inside the blog page
+  if (document.getElementById("blogPostsContainer")) {
+    // Fetch blog posts data
+    fetch("/data/blog-posts.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const container = document.getElementById("blogPostsContainer");
+
+        // Create HTML for each blog posts
+        let blog = '<div class="blogPostsList">';
+
+        data.forEach((post) => {
+          blog += `
+          <a href="${post.file}" class="blogPostsFile">
+            <h2>${post.title}</h2>
+            <p class="blogPostsDate">${post.date}</p>
+            <p>${post.excerpt}</p>
+          </a>
+          `;
+        });
+
+        blog += "</div>";
+        container.innerHTML = blog;
+      })
+      .catch((error) => {
+        console.error("Error reading blog posts: ", error);
+        document.getElementById("blogPostsContainer").innerHTML =
+          "<p>Failed to load blog posts.</p>";
+      });
+  }
+});
